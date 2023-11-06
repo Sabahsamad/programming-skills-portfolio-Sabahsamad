547259dcfb38d530a3c3876678eb5971ee8554a1
Sabah Samad <sabahsamadbsu@gmail.com>
2023-11-06 10:47:33 +0400

    Exercise3.py

---
+++ Chapter 2- Variables & Comments/Exercise3.py
@@ -0,0 +1,13 @@
+name = "\tSabah Samad\n"
+
+print("Unmodified:")
+print(name)
+
+print("\nUsing lstrip():")
+print(name.lstrip())
+
+print("\nUsing rstrip():")
+print(name.rstrip())
+
+print("\nUsing strip():")
+print(name.strip())
