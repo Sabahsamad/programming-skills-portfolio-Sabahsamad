5d07dc3d5793bd9285773b8386aff93282c6c6f7
Sabah Samad <sabahsamadbsu@gmail.com>
2023-11-06 10:50:31 +0400

    Exercise5.py

---
+++ Chapter 2- Variables & Comments/Exercise5.py
@@ -0,0 +1,4 @@
+print("USB stick price is $10. \n Note: Girl has $50.")
+quantity = int(input ("how many can the girl buy:"))
+price = int(50 - (quantity*6))
+print ("the girl's change is:\n", price)
