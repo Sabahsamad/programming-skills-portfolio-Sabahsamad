d7e42743173cd6b58bcdf9f887e9fbbb07fdbd4a
Sabah Samad <sabahsamadbsu@gmail.com>
2023-11-12 03:39:04 +0400

    Exercise7.py

---
+++ Chapter 3- Data Structures/Exercise7.py
@@ -0,0 +1,20 @@
+places= ['Bahrain', 'Kuwait', 'Saudi Arabia', 'Oman', 'Qatar'] #original list
+print(places) #prints the original list
+
+print(sorted(places)) #prints the list in alphabetic order
+
+print(places) #print the original list
+
+print(sorted(places, reverse=True)) # prints list in Reverse alphabetic order
+
+print(places) # shows the original list
+
+places.reverse() # make the list in reverse order
+print(places) # prints the list in reverse order
+places.reverse() # changes the reversed list to its original state
+print(places) # print the list
+
+places.sort()
+print(places)
+places.sort(reverse=True)
+print(places)
