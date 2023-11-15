59a477dfa4601f8b61c3d34c71a7c5966525d6cd
Sabah Samad <sabahsamadbsu@gmail.com>
2023-11-15 11:06:12 +0400

    Exercise1.py

---
+++ Chapter 4- Control Flow/Exercise1.py
@@ -0,0 +1,12 @@
+#Exercise 1: Names :ballot_box_with_check:
+'''Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+•Write an if statement to test whether the aliens color is green. If it is, print a message that the player just earned 5 points.
+•Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)'''
+
+alien_color = "Red"
+if alien_color == "Red":
+    print("Congrats! You have earned 12 points!")
+
+alien_color = "Red"
+if alien_color == "Red":
+    print("Congrats! You have earned 12 points!")
